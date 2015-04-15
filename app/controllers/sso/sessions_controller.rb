2dbdf79e6e2d09e60b01168c1e27fa4f07bc57f7
John Wong <john@flexnode.com>
2015-04-15 17:32:21 +0800

    Cleanup application controller issue

--- app/controllers/sso/sessions_controller.rb
+++ app/controllers/sso/sessions_controller.rb
@@ -1,29 +1,31 @@
-class Sso::SessionsController < ApplicationController
+module Sso
+  class SessionsController < ApplicationController
 
-  before_action :authenticate_user!, only: :show
-  before_action :doorkeeper_authorize!, only: :create
-  before_action :find_user, only: :create
+    before_action :authenticate_user!, only: :show
+    before_action :doorkeeper_authorize!, only: :create
+    before_action :find_user, only: :create
 
-  # TODO: Security issue?
-  protect_from_forgery with: :null_session
+    # TODO: Security issue?
+    protect_from_forgery with: :null_session
 
-  respond_to :json
+    respond_to :json
 
-  # Returns a 200 if access is granted
-  def show
-    render :nothing => true
+    # Returns a 200 if access is granted
+    def show
+      render :nothing => true
+    end
+
+    # Generate an SSO:Session
+    def create
+      @session  = Sso::Session.generate(@user, doorkeeper_token, params )
+      respond_with @session, :location => sso.sessions_url
+    end
+
+    protected
+
+    def find_user
+      @user = User.find(doorkeeper_token.resource_owner_id)
+    end
+
   end
-
-  # Generate an SSO:Session
-  def create
-    @session  = Sso::Session.generate(@user, doorkeeper_token, params )
-    respond_with @session, :location => sso.sessions_url
-  end
-
-  protected
-
-  def find_user
-    @user = User.find(doorkeeper_token.resource_owner_id)
-  end
-
 end
--- app/helpers/sso/application_helper.rb
+++ app/helpers/sso/application_helper.rb
@@ -1,4 +1,24 @@
 module Sso
   module ApplicationHelper
+    # Can search for named routes directly in the main app, omitting
+    # the "main_app." prefix
+    def method_missing method, *args, &block
+      if main_app_url_helper?(method)
+        main_app.send(method, *args)
+      else
+        super
+      end
+    end
+
+    def respond_to?(method)
+      main_app_url_helper?(method) or super
+    end
+
+   private
+
+    def main_app_url_helper?(method)
+      (method.to_s.end_with?('_path') or method.to_s.end_with?('_url')) and
+        main_app.respond_to?(method)
+    end
   end
 end
