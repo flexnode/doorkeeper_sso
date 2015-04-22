3e115794c31ba7e77ffa751b67627d388fff3d70
Edham Arief / edthix <edthix@gmail.com>
2015-04-22 23:46:34 +0800

    Specs for warden hooks

---
+++ spec/lib/sso/warden/hooks/after_authentication_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+RSpec.describe Sso::Warden::Hooks::AfterAuthentication do
+  #include Warden::Test::Helpers
+
+  let(:user) { Fabricate(:user) }
+  let(:warden_mock) { double }
+  let(:attributes) { { :ip => "202.188.0.133", :agent => "Chrome", format: :json } }
+
+  let(:after_authentication) { Sso::Warden::Hooks::AfterAuthentication.
+                               new(user: user, warden: warden_mock, options: {} ) }
+
+
+  let(:master_sso_session) { Sso::Session.generate_master(user, attributes) }
+  let(:access_token) { Fabricate("Doorkeeper::AccessToken",
+                                 resource_owner_id: user.id) }
+  let(:access_grant) { Fabricate('Doorkeeper::AccessGrant',
+                                 resource_owner_id: user.id,
+                                 redirect_uri: 'http://localhost:3002/oauth/callback'
+                                ) }
+
+  before do
+    master_sso_session.access_token_id = access_token.id
+    master_sso_session.access_grant_id = access_grant.id
+    master_sso_session.save
+  end
+
+  describe 'attributes' do
+    it do
+      expect(after_authentication.user).to eq user
+      expect(after_authentication.warden).to eq warden_mock
+      expect(after_authentication.options).to eq({})
+    end
+  end
+
+  pending "#call"
+end
