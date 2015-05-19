9050f642bad5af3891cb6835435e69d06e298ec2
John Wong <john@flexnode.com>
2015-05-19 14:14:20 +0800

    Setup generators

--- lib/sso/engine.rb
+++ lib/sso/engine.rb
@@ -2,6 +2,18 @@ module Sso
   class Engine < ::Rails::Engine
     isolate_namespace Sso
 
+    # New test framework integration
+    config.generators do |g|
+      g.test_framework  :rspec,
+                        :fixtures => true,
+                        :view_specs => false,
+                        :helper_specs => false,
+                        :routing_specs => false,
+                        :controller_specs => true,
+                        :request_specs => false
+      g.fixture_replacement :fabrication
+    end
+
     config.after_initialize do
 
       ::Doorkeeper::TokensController.send(:include, AbstractController::Callbacks)
