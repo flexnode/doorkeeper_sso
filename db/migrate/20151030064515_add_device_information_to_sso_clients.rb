cf4115cf7e66cd1c12bee20fd261013f9e81cf78
John Wong <john@flexnode.com>
2015-11-05 13:50:25 +0800

    Modify migration so client always has a random token created

--- db/migrate/20151030064515_add_device_information_to_sso_clients.rb
+++ db/migrate/20151030064515_add_device_information_to_sso_clients.rb
@@ -4,7 +4,13 @@ class AddDeviceInformationToSsoClients < ActiveRecord::Migration
       t.string      "device_os"
       t.string      "device_os_version"
       t.string      "device_model"
-      t.string      "random_token", null: false
+      t.string      "random_token"
     end
+
+    Sso::Client.find_each do |client|
+      client.save
+    end
+
+    change_column :sso_clients, :random_token, :string, :null => true
   end
 end
